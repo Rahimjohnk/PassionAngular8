73f6635f8e24f3d23faf086afd128e3936ef30e9
Rahim john <k.rahim001@gmail.com>
2020-01-18 21:51:37 +0530

    basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template
   styleUrls: ['./app.component.css']
+  // style
 })
 export class AppComponent {
   title = 'passionAngular8';
